7ece6d404bcc8aee3e6d76f4592d1a41f4828841
Nicollas <nicollason@gmail.com>
2024-06-10 23:01:24 -0300

    js

---
+++ aula 06/index.js
@@ -0,0 +1,11 @@
+//Não podemos redeclarar variaveis com let
+//Não utilize var, utilize LET
+
+
+let nome = 'João';
+
+console.log(nome, 'nasceu em 1984.')
+console.log('Em 2000', nome ,'conheceu maria.')
+console.log(nome, 'casou-se com Maria em 2012.')
+console.log('Maria teve 1 filho com', nome ,'em 2015.')
+console.log('O filho de', nome, 'se chama Eduardo.')
